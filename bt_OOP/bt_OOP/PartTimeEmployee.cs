ec43f2af54b095ac1829eccc41533e7bb2aa9eb1
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-03-05 01:13:14 +0700

    tao class PartTimeEmployee va tao bien voi cac method

---
+++ bt_OOP/bt_OOP/PartTimeEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt_OOP
+{
+    internal class PartTimeEmployee : Employee
+    {
+        private int workingHours { get; set; }
+
+        public PartTimeEmployee(string name, int paymentPerHour, int workingHours) : base(name, paymentPerHour)
+        {
+            this.workingHours = workingHours;
+        }
+
+        public override int calculateSalary()
+        {
+            return workingHours * getPaymentPerHour();
+        }
+
+    }
+}
