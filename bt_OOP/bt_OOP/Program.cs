a1fc6fda57757ab241e798cf4705ac43b1a9fdb1
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-03-05 02:15:07 +0700

    them main

--- bt_OOP/bt_OOP/Program.cs
+++ bt_OOP/bt_OOP/Program.cs
@@ -10,6 +10,20 @@ namespace bt_OOP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("====================================");
+            Console.WriteLine("||Nguyen Vu Khanh - 22115053122118||");
+            Console.WriteLine("====================================");
+            PartTimeEmployee pte = new PartTimeEmployee("Nguyen Vu khanh",18000,5);
+            PartTimeEmployee pte2 = new PartTimeEmployee("Tran Dinh Quy", 18000,6);
+
+            FullTimeEmployee fte = new FullTimeEmployee("Nguyen Dinh Duc", 20000);
+            FullTimeEmployee fte2 = new FullTimeEmployee("Le Thi Tra Giang", 30000);
+
+            Console.WriteLine(pte.toString());
+            Console.WriteLine(pte2.toString());
+            Console.WriteLine(fte.toString());
+            Console.WriteLine(fte2.toString());
+
         }
     }
 }
