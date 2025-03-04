d0d5dd50bc34679b4cf75aabbc60c2c4b0520e2e
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-03-05 02:39:27 +0700

    sua ten bien o main va anh chup code voi ket qua

--- bt_OOP/bt_OOP/Program.cs
+++ bt_OOP/bt_OOP/Program.cs
@@ -13,16 +13,16 @@ namespace bt_OOP
             Console.WriteLine("====================================");
             Console.WriteLine("||Nguyen Vu Khanh - 22115053122118||");
             Console.WriteLine("====================================");
-            PartTimeEmployee pte = new PartTimeEmployee("Nguyen Vu khanh",18000,5);
-            PartTimeEmployee pte2 = new PartTimeEmployee("Tran Dinh Quy", 18000,6);
+            PartTimeEmployee pte_118 = new PartTimeEmployee("Nguyen Vu khanh",18000,5);
+            PartTimeEmployee pte2_118 = new PartTimeEmployee("Tran Dinh Quy", 18000,6);
 
-            FullTimeEmployee fte = new FullTimeEmployee("Nguyen Dinh Duc", 20000);
-            FullTimeEmployee fte2 = new FullTimeEmployee("Le Thi Tra Giang", 30000);
+            FullTimeEmployee fte_118 = new FullTimeEmployee("Nguyen Dinh Duc", 20000);
+            FullTimeEmployee fte2_118 = new FullTimeEmployee("Le Thi Tra Giang", 30000);
 
-            Console.WriteLine(pte.toString());
-            Console.WriteLine(pte2.toString());
-            Console.WriteLine(fte.toString());
-            Console.WriteLine(fte2.toString());
+            Console.WriteLine(pte_118.toString());
+            Console.WriteLine(pte2_118.toString());
+            Console.WriteLine(fte_118.toString());
+            Console.WriteLine(fte2_118.toString());
 
         }
     }
