12f23bcddceace5b399c7b127b70bdeadbb72b3d
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-03-05 01:34:46 +0700

    tao class FullTimeEmployee

---
+++ bt_OOP/bt_OOP/FullTimeEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt_OOP
+{
+    internal class FullTimeEmployee : Employee
+    {
+        public FullTimeEmployee(string name_118, int paymentPerHour_118) : base(name_118, paymentPerHour_118)
+        {
+
+        }
+
+        public override int calculateSalary()
+        {
+            return 8 * getPaymentPerHour();
+        }
+    }
+}
